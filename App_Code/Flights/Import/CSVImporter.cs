86ea257d2e4d80be7c74d699f328803dc0ad351f
ericberman <ericberman@users.noreply.github.com>
2017-10-11 15:51:15 -0700

    a few email addresses missed

--- App_Code/Flights/Import/CSVImporter.cs
+++ App_Code/Flights/Import/CSVImporter.cs
@@ -10,7 +10,7 @@ using JouniHeikniemi.Tools.Text;
 /******************************************************
  * 
  * Copyright (c) 2008-2016 MyFlightbook LLC
- * Contact myflightbook@gmail.com for more information
+ * Contact myflightbook-at-gmail.com for more information
  *
 *******************************************************/
 
