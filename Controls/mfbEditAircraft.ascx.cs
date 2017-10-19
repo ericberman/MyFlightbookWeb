c30e5a1e61a34c05cc287e2e6c123913b6753a3c
ericberman <ericberman@users.noreply.github.com>
2017-10-19 14:42:55 -0700

    SVN 5955 - fix break in new aircraft.
    Fix last check-in - it broke for new aircraft.

--- Controls/mfbEditAircraft.ascx.cs
+++ Controls/mfbEditAircraft.ascx.cs
@@ -252,9 +252,12 @@ public partial class Controls_mfbEditAircraft : System.Web.UI.UserControl
         if (AdminMode)
             lst.Add(String.Format(CultureInfo.CurrentCulture, "Users = {0}", String.Join(", ", Stats.UserNames)));
 
-        lnkViewTotals.Visible = !AdminMode && m_ac.AircraftID != Aircraft.idAircraftUnknown;
-        FlightQuery fq = new FlightQuery(Page.User.Identity.Name) { AircraftIDList = new int[] { m_ac.AircraftID } };
-        lnkViewTotals.NavigateUrl = String.Format(CultureInfo.InvariantCulture, "~/Member/LogbookNew.aspx?ft=Totals&fq={0}", HttpUtility.UrlEncode(Convert.ToBase64String(fq.ToJSONString().Compress())));
+        lnkViewTotals.Visible = !AdminMode && !m_ac.IsNew;
+        if (!m_ac.IsNew)
+        {
+            FlightQuery fq = new FlightQuery(Page.User.Identity.Name) { AircraftIDList = new int[] { m_ac.AircraftID } };
+            lnkViewTotals.NavigateUrl = String.Format(CultureInfo.InvariantCulture, "~/Member/LogbookNew.aspx?ft=Totals&fq={0}", HttpUtility.UrlEncode(Convert.ToBase64String(fq.ToJSONString().Compress())));
+        }
 
         rptStats.DataSource = lst;
         rptStats.DataBind();
