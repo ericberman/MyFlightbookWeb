92a444656ac28fbbe3b11f1a46b65b4cf88fe7f1
ericberman <ericberman@users.noreply.github.com>
2018-01-31 16:14:45 -0800

    SVN 6077: A bunch of tables are missing foreign-key constraints that really should be present.

---
+++ Support/2018-01-31-Missing Foreign Keys.sql
@@ -0,0 +1,135 @@
+﻿ALTER TABLE `logbook`.`aircraft` 
+DROP FOREIGN KEY `model`,
+DROP FOREIGN KEY `instance`;
+ALTER TABLE `logbook`.`aircraft` 
+ADD CONSTRAINT `model`
+  FOREIGN KEY (`idmodel`)
+  REFERENCES `logbook`.`models` (`idmodel`)
+  ON DELETE RESTRICT
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `instance`
+  FOREIGN KEY (`InstanceType`)
+  REFERENCES `logbook`.`aircraftinstancetypes` (`ID`)
+  ON DELETE RESTRICT
+  ON UPDATE NO ACTION;
+
+  ALTER TABLE `logbook`.`aircrafttombstones` 
+CHANGE COLUMN `idMappedAircraft` `idMappedAircraft` INT(10) UNSIGNED NOT NULL COMMENT 'The ID of the aircraft with which it was merged' ;
+
+ALTER TABLE `logbook`.`aircrafttombstones` 
+ADD CONSTRAINT `mappedaircraft`
+  FOREIGN KEY (`idMappedAircraft`)
+  REFERENCES `logbook`.`aircraft` (`idaircraft`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`badges` 
+ADD INDEX `Users_idx` (`Username` ASC);
+ALTER TABLE `logbook`.`badges` 
+ADD CONSTRAINT `Users`
+  FOREIGN KEY (`Username`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`clubmembers` 
+ADD CONSTRAINT `fkuser`
+  FOREIGN KEY (`username`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`customcurrency` 
+ADD CONSTRAINT `fkcustomcurrencyuser`
+  FOREIGN KEY (`username`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`deadlines` 
+ADD CONSTRAINT `fkdeadlineuser`
+  FOREIGN KEY (`username`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+
+ALTER TABLE `logbook`.`earnedgratuities` 
+ADD CONSTRAINT `fkearnedgratuityuser`
+  FOREIGN KEY (`username`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`endorsements` 
+ADD INDEX `fkendorsement_idx` (`Student` ASC);
+ALTER TABLE `logbook`.`endorsements` 
+ADD CONSTRAINT `fkendorsement`
+  FOREIGN KEY (`Student`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+  ALTER TABLE `logbook`.`flightproperties` 
+ADD CONSTRAINT `fkPropType`
+  FOREIGN KEY (`idPropType`)
+  REFERENCES `logbook`.`custompropertytypes` (`idPropType`)
+  ON DELETE RESTRICT
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`flights` 
+ADD CONSTRAINT `fkFlightAircraft`
+  FOREIGN KEY (`idaircraft`)
+  REFERENCES `logbook`.`aircraft` (`idaircraft`)
+  ON DELETE RESTRICT
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`models` 
+ADD CONSTRAINT `fkmodelsMan`
+  FOREIGN KEY (`idmanufacturer`)
+  REFERENCES `logbook`.`manufacturers` (`idManufacturer`)
+  ON DELETE RESTRICT
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fkmodelscatclass`
+  FOREIGN KEY (`idcategoryclass`)
+  REFERENCES `logbook`.`categoryclass` (`idCatClass`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`students` 
+ADD INDEX `fkCFIName_idx` (`CFIName` ASC),
+ADD INDEX `fkStudenName_idx` (`StudentName` ASC);
+ALTER TABLE `logbook`.`students` 
+ADD CONSTRAINT `fkStudenName`
+  FOREIGN KEY (`StudentName`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fkCFIName`
+  FOREIGN KEY (`CFIName`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`useraircraft` 
+ADD INDEX `fkuseraircraftUName_idx` (`userName` ASC);
+ALTER TABLE `logbook`.`useraircraft` 
+ADD CONSTRAINT `fkuseraircraftUName`
+  FOREIGN KEY (`userName`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fkuseraircraftAc`
+  FOREIGN KEY (`idAircraft`)
+  REFERENCES `logbook`.`aircraft` (`idaircraft`)
+  ON DELETE RESTRICT
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `logbook`.`usersinroles` 
+ADD CONSTRAINT `fkusersinrolesUsername`
+  FOREIGN KEY (`Username`)
+  REFERENCES `logbook`.`users` (`Username`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+
