89d6b49a7e0d113dfa510c7ed80f6936c76750b2
ericberman <ericberman@users.noreply.github.com>
2018-07-31 08:56:34 -0700

    Issue #155: Part 135 milestone progress should use part 135 definition of cross-country (non-local)

--- App_Code/Flights/Ratings/Part135Ratings.cs
+++ App_Code/Flights/Ratings/Part135Ratings.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using MyFlightbook.Airports;
+using MyFlightbook.FlightCurrency;
+using System;
 using System.Collections.ObjectModel;
 using System.Globalization;
-using MyFlightbook.FlightCurrency;
+using System.Text.RegularExpressions;
 
 /******************************************************
  * 
- * Copyright (c) 2013-2017 MyFlightbook LLC
+ * Copyright (c) 2013-2018 MyFlightbook LLC
  * Contact myflightbook-at-gmail.com for more information
  *
 *******************************************************/
@@ -26,6 +28,29 @@ namespace MyFlightbook.MilestoneProgress
                 new Part135243b(),
                 new Part135243c()
             };
+        }
+
+        /// <summary>
+        /// Determines the amount of part 135 cross-country time in this flight.
+        /// Part 135 time is cross country if it goes from airport A to airport B
+        /// We mimic the FlightQuery definition of non-local here, which looks at the length of the route field and verifies that it matches neither
+        /// ABCD-ABCD or ABC-ABC.  (If it's simply ABC or simply ABCD, then it fails the length test)
+        /// If it is non-local, then we use MAX(total time, cross-country).
+        /// </summary>
+        /// <param name="cfr">The flight</param>
+        /// <returns>The amount of cross-country time to contribute</returns>
+        public static decimal Part135CrossCountry(ExaminerFlightRow cfr)
+        {
+            if (cfr == null)
+                return 0.0M;
+            if (cfr.Route == null || cfr.Route.Length <= airport.maxCodeLength)
+                return cfr.XC;
+
+            // Mimic
+            Regex r4x4 = new Regex(String.Format(CultureInfo.InvariantCulture, "^{0}[^a-zA-Z0-9]*{0}$", cfr.Route.Substring(0, 4)), RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            Regex r3x3 = new Regex(String.Format(CultureInfo.InvariantCulture, "^{0}[^a-zA-Z0-9]*{0}$", cfr.Route.Substring(0, 3)), RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            return (r4x4.IsMatch(cfr.Route) || r3x3.IsMatch(cfr.Route)) ? cfr.XC : Math.Max(cfr.XC, cfr.Total);
         }
     }
 
@@ -52,8 +77,8 @@ namespace MyFlightbook.MilestoneProgress
             Title = Resources.MilestoneProgress.Title135243PIC;
             GeneralDisclaimer = Branding.ReBrand(Resources.MilestoneProgress.Part135PICDisclaimer);
             miMinTimeAsPilot = new MilestoneItem(String.Format(CultureInfo.CurrentCulture, Resources.MilestoneProgress.Part135PICMinTime, minTime), szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minTime);
-            miMinXCTime = new MilestoneItem(String.Format(CultureInfo.CurrentCulture, Resources.MilestoneProgress.Part135PICXCMinTime, minXCTime), szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minXCTime);
-            miMinXCNightTime = new MilestoneItem(Resources.MilestoneProgress.Part135PICNightXCMinTime, szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minXCNightTime);
+            miMinXCTime = new MilestoneItem(String.Format(CultureInfo.CurrentCulture, Resources.MilestoneProgress.Part135PICXCMinTime, minXCTime), szFAR, Resources.MilestoneProgress.Part135XCNote, MilestoneItem.MilestoneType.Time, minXCTime);
+            miMinXCNightTime = new MilestoneItem(Resources.MilestoneProgress.Part135PICNightXCMinTime, szFAR, Resources.MilestoneProgress.Part135XCNote, MilestoneItem.MilestoneType.Time, minXCNightTime);
         }
 
         public override void ExamineFlight(ExaminerFlightRow cfr)
@@ -64,9 +89,11 @@ namespace MyFlightbook.MilestoneProgress
             if (!cfr.fIsRealAircraft)
                 return;
 
+            decimal part135XC = Part135Milestones.Part135CrossCountry(cfr);
+
             miMinTimeAsPilot.AddEvent(cfr.Total);
-            miMinXCTime.AddEvent(cfr.XC);
-            miMinXCNightTime.AddEvent(Math.Min(cfr.XC, cfr.Night));
+            miMinXCTime.AddEvent(part135XC);
+            miMinXCNightTime.AddEvent(Math.Min(part135XC, cfr.Night));
         }
 
         public override Collection<MilestoneItem> Milestones
@@ -104,7 +131,7 @@ namespace MyFlightbook.MilestoneProgress
             Title = Resources.MilestoneProgress.Title135243PICIFR;
             GeneralDisclaimer = Branding.ReBrand(Resources.MilestoneProgress.Part135PICDisclaimer);
             miMinTimeAsPilot = new MilestoneItem(String.Format(CultureInfo.CurrentCulture, Resources.MilestoneProgress.Part135PICMinTime, minTime), szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minTime);
-            miMinXCTime = new MilestoneItem(String.Format(CultureInfo.CurrentCulture, Resources.MilestoneProgress.Part135PICXCMinTime, minXCTime), szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minXCTime);
+            miMinXCTime = new MilestoneItem(String.Format(CultureInfo.CurrentCulture, Resources.MilestoneProgress.Part135PICXCMinTime, minXCTime), szFAR, Resources.MilestoneProgress.Part135XCNote, MilestoneItem.MilestoneType.Time, minXCTime);
             miMinNightTime = new MilestoneItem(Resources.MilestoneProgress.Part135PICIFRNightTime, szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minNightTime);
             miMinIFRTime = new MilestoneItem(Resources.MilestoneProgress.Part135PICIFRTime, szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minIFRTime);
             miMinIFRAircraftTime = new MilestoneItem(Resources.MilestoneProgress.Part135PICIFRTimeInFlight, szFAR, string.Empty, MilestoneItem.MilestoneType.Time, minIFRAircraftTime);
@@ -122,8 +149,10 @@ namespace MyFlightbook.MilestoneProgress
             if (!cfr.fIsRealAircraft)
                 return;
 
+            decimal part135XC = Part135Milestones.Part135CrossCountry(cfr);
+
             miMinTimeAsPilot.AddEvent(cfr.Total);
-            miMinXCTime.AddEvent(cfr.XC);
+            miMinXCTime.AddEvent(part135XC);
             miMinNightTime.AddEvent(cfr.Night);
             miMinIFRAircraftTime.AddEvent(IMCTime);
         }
