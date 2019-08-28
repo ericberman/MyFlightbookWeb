0869061c978ca1a7b12fda87a03172663f45306e
ericberman <ericberman@users.noreply.github.com>
2019-08-28 11:28:21 -0700

    Fix issue #364: deleting an image in a group hides the display of controls for other images in the group.

--- Controls/mfbEditableImage.ascx.cs
+++ Controls/mfbEditableImage.ascx.cs
@@ -159,7 +159,8 @@ function EditComment(idStatic, idEdit) {
 
         lnkAnnotate.NavigateUrl = String.Format(CultureInfo.InvariantCulture, "javascript:EditComment('{0}', '{1}');", pnlStatic.ClientID, pnlEdit.ClientID);
 
-        divActions.Visible = lnkAnnotate.Visible || lnkDelete.Visible || lnkMakeDefault.Visible || lnkZoom.Visible;
+        if (mfbii != null)
+            divActions.Visible = lnkAnnotate.Visible || lnkDelete.Visible || lnkMakeDefault.Visible || lnkZoom.Visible;
     }
 
     /// <summary>
