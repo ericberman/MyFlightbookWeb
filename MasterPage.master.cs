3008a13aa537bbee3bacbe06255b29add33dbc48
ericberman <ericberman@users.noreply.github.com>
2019-09-18 08:16:54 -0700

    SVN 6786: Relax 1400px width when printing.

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -192,7 +192,7 @@ public partial class MasterPage : System.Web.UI.MasterPage
             lnkPrivacy.Text = String.Format(CultureInfo.CurrentCulture, Resources.LocalizedText.PrivacyPolicyHeader, Branding.CurrentBrand.AppName);
 
             lnkAppleIcon.Href = ResolveUrl("~/images/apple-touch-icon.png");
-            cssMain.Href = "~/Public/stylesheet.css".ToAbsoluteURL(Request).ToString() + "?v=13";    // to enable forced reload
+            cssMain.Href = "~/Public/stylesheet.css".ToAbsoluteURL(Request).ToString() + "?v=14";    // to enable forced reload
             cssMobile.Visible = mfbHeader.IsMobile = MfbFooter.IsMobile = IsMobileSession();
             cssMobile.Href = ResolveUrl("~/Public/CSS/MobileSheet.css?v=7");
             string szStyle = Branding.CurrentBrand.StyleSheet;
