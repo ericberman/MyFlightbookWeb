bc557ff70bbb53382bbccd81e18206e4d6dd57c0
ericberman <ericberman@users.noreply.github.com>
2020-03-06 13:24:37 -0800

    SVN 7051: Clean up disposable issue in CSVAnalyzer

--- MyFlightbook.Web/AppCode/CSV/CSVAnalyzer.cs
+++ MyFlightbook.Web/AppCode/CSV/CSVAnalyzer.cs
@@ -1,5 +1,4 @@
 ﻿using JouniHeikniemi.Tools.Text;
-using MyFlightbook;
 using MyFlightbook.CSV;
 using System;
 using System.Collections.Generic;
@@ -10,7 +9,7 @@ using System.Text;
 
 /******************************************************
  * 
- * Copyright (c) 2007-2017 MyFlightbook LLC
+ * Copyright (c) 2007-2020 MyFlightbook LLC
  * Contact myflightbook-at-gmail.com for more information
  *
 *******************************************************/
@@ -82,8 +81,7 @@ namespace MyFlightbook
         protected CSVAnalyzer()
         {
             Status = CSVStatus.OK;
-            m_dt = new DataTable();
-            m_dt.Locale = CultureInfo.CurrentCulture ;
+            m_dt = new DataTable { Locale = CultureInfo.CurrentCulture };
         }
 
         public CSVAnalyzer(Stream sIn) : this() { Validate(sIn); }
@@ -99,116 +97,118 @@ namespace MyFlightbook
             sbAudit = new StringBuilder();
 
             // First pass - see how many columns we should have, see if all is good.
-            CSVReader reader = new CSVReader(sIn);
-            try
+            using (CSVReader reader = new CSVReader(sIn))
             {
-                string[] rgszHeader = reader.GetCSVLine(true);
-                int cColumns = rgszHeader.Length;
-                if (cColumns == 0)
-                    throw new MyFlightbookValidationException(Resources.LogbookEntry.errImportNoHeaders);
-
-                // look for empty or duplicate columns
-                Dictionary<string, string> dictHeaders = new Dictionary<string, string>();
-                int iCol = 0;
-                foreach (string sz in rgszHeader)
+                try
                 {
-                    string szH = sz.Trim();
-                    iCol++;
-                    if (String.IsNullOrWhiteSpace(szH))
-                        throw new MyFlightbookValidationException(String.Format(CultureInfo.CurrentCulture, Resources.LogbookEntry.errImportEmptyColumn, iCol));
-                    if (dictHeaders.ContainsKey(szH))
-                        throw new MyFlightbookValidationException(String.Format(CultureInfo.CurrentCulture, Resources.LogbookEntry.errImportDuplicateColumn, sz));
-                    dictHeaders[szH] = szH;
-                    Data.Columns.Add(new DataColumn(szH, typeof(string)));
-                }
+                    string[] rgszHeader = reader.GetCSVLine(true);
+                    int cColumns = rgszHeader.Length;
+                    if (cColumns == 0)
+                        throw new MyFlightbookValidationException(Resources.LogbookEntry.errImportNoHeaders);
 
-                sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvauditColumnsFound, cColumns);
-                sbAudit.AppendLine();
-
-                int iRow = 0;
-                string[] rgszRow = null;
-                bool fShortRowsFound = false;
-                int cColsSoFar = 0;
-                while ((rgszRow = reader.GetCSVLine()) != null)
-                {
-                    ++iRow;
-                    int cColsThisRow = rgszRow.Length;  // number of columns in this row
-                    if (cColsThisRow == 0)
+                    // look for empty or duplicate columns
+                    Dictionary<string, string> dictHeaders = new Dictionary<string, string>();
+                    int iCol = 0;
+                    foreach (string sz in rgszHeader)
                     {
-                        sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditEmptyRow, iRow);
-                        sbAudit.AppendLine();
-                        continue;
+                        string szH = sz.Trim();
+                        iCol++;
+                        if (String.IsNullOrWhiteSpace(szH))
+                            throw new MyFlightbookValidationException(String.Format(CultureInfo.CurrentCulture, Resources.LogbookEntry.errImportEmptyColumn, iCol));
+                        if (dictHeaders.ContainsKey(szH))
+                            throw new MyFlightbookValidationException(String.Format(CultureInfo.CurrentCulture, Resources.LogbookEntry.errImportDuplicateColumn, sz));
+                        dictHeaders[szH] = szH;
+                        Data.Columns.Add(new DataColumn(szH, typeof(string)));
                     }
 
-                    int cColsStarting = cColsSoFar;     // number of columns seen so far in this row
-                    cColsSoFar += (cColsSoFar == 0 ? 0 : -1) + cColsThisRow; // if we've already got some columns in progress, subtract one to merge the first column of this row with the last column of the prior row.
-                    if (cColsSoFar > cColumns)
+                    sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvauditColumnsFound, cColumns);
+                    sbAudit.AppendLine();
+
+                    int iRow = 0;
+                    string[] rgszRow = null;
+                    bool fShortRowsFound = false;
+                    int cColsSoFar = 0;
+                    while ((rgszRow = reader.GetCSVLine()) != null)
                     {
-                        sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditAppendedRowTooLong, iRow, cColsThisRow, cColsSoFar);
-                        sbAudit.AppendLine();
-                        cColsSoFar = cColsStarting = 0;
-                    }
-
-                    if (rgszRow.Length > cColumns)
-                        throw new MyFlightbookValidationException(String.Format(CultureInfo.CurrentCulture, Resources.LogbookEntry.errImportTooManyColumns, iRow, rgszRow.Length, cColumns));
-
-                    if (cColsStarting == 0)
-                        Data.Rows.Add(Data.NewRow());
-                    DataRow dr = Data.Rows[Data.Rows.Count - 1];
-
-                    for (int i = 0; i < cColsThisRow; i++)
-                    {
-                        if (cColsStarting == 0)
-                            dr[cColsStarting + i] = rgszRow[i];
-                        else if (i == 0)
-                            dr[cColsStarting - 1] = String.Format(CultureInfo.CurrentCulture, Resources.LocalizedText.LocalizedJoinWithSpace, dr[cColsStarting - 1], rgszRow[0]);
-                        else
-                            dr[cColsStarting - 1 + i] = rgszRow[i];
-                    }
-
-                    if (cColsSoFar == cColumns)
-                    {
-                        if (cColsStarting > 0)
+                        ++iRow;
+                        int cColsThisRow = rgszRow.Length;  // number of columns in this row
+                        if (cColsThisRow == 0)
                         {
-                            sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditRowMergedWithPrior, iRow, rgszRow.Length);
+                            sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditEmptyRow, iRow);
+                            sbAudit.AppendLine();
+                            continue;
+                        }
+
+                        int cColsStarting = cColsSoFar;     // number of columns seen so far in this row
+                        cColsSoFar += (cColsSoFar == 0 ? 0 : -1) + cColsThisRow; // if we've already got some columns in progress, subtract one to merge the first column of this row with the last column of the prior row.
+                        if (cColsSoFar > cColumns)
+                        {
+                            sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditAppendedRowTooLong, iRow, cColsThisRow, cColsSoFar);
+                            sbAudit.AppendLine();
+                            cColsSoFar = cColsStarting = 0;
+                        }
+
+                        if (rgszRow.Length > cColumns)
+                            throw new MyFlightbookValidationException(String.Format(CultureInfo.CurrentCulture, Resources.LogbookEntry.errImportTooManyColumns, iRow, rgszRow.Length, cColumns));
+
+                        if (cColsStarting == 0)
+                            Data.Rows.Add(Data.NewRow());
+                        DataRow dr = Data.Rows[Data.Rows.Count - 1];
+
+                        for (int i = 0; i < cColsThisRow; i++)
+                        {
+                            if (cColsStarting == 0)
+                                dr[cColsStarting + i] = rgszRow[i];
+                            else if (i == 0)
+                                dr[cColsStarting - 1] = String.Format(CultureInfo.CurrentCulture, Resources.LocalizedText.LocalizedJoinWithSpace, dr[cColsStarting - 1], rgszRow[0]);
+                            else
+                                dr[cColsStarting - 1 + i] = rgszRow[i];
+                        }
+
+                        if (cColsSoFar == cColumns)
+                        {
+                            if (cColsStarting > 0)
+                            {
+                                sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditRowMergedWithPrior, iRow, rgszRow.Length);
+                                sbAudit.AppendLine();
+                            }
+                            cColsSoFar = 0;
+                        }
+                        else if (rgszRow.Length < cColumns)
+                        {
+                            fShortRowsFound = true;
+                            sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditRowIncomplete, iRow, rgszRow.Length, cColumns);
                             sbAudit.AppendLine();
                         }
-                        cColsSoFar = 0;
                     }
-                    else if (rgszRow.Length < cColumns)
-                    {
-                        fShortRowsFound = true;
-                        sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditRowIncomplete, iRow, rgszRow.Length, cColumns);
-                        sbAudit.AppendLine();
-                    }
-                }
 
-                if (Data.Rows.Count == 0) // no data rows found
-                    throw new MyFlightbookValidationException(Resources.LogbookEntry.errImportNoData);
+                    if (Data.Rows.Count == 0) // no data rows found
+                        throw new MyFlightbookValidationException(Resources.LogbookEntry.errImportNoData);
 
-                sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditTotalRows, Data.Rows.Count);
-                sbAudit.AppendLine();
-
-                if (!fShortRowsFound)
-                {
-                    sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditSuccess);
+                    sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditTotalRows, Data.Rows.Count);
                     sbAudit.AppendLine();
-                    return Status = CSVStatus.OK;
+
+                    if (!fShortRowsFound)
+                    {
+                        sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditSuccess);
+                        sbAudit.AppendLine();
+                        return Status = CSVStatus.OK;
+                    }
+                    else
+                    {
+                        return Status = CSVStatus.Fixed;
+                    }
                 }
-                else
+                catch (CSVReaderInvalidCSVException ex)
                 {
-                    return Status = CSVStatus.Fixed;
+                    sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditErrorFound, ex.Message);
+                    sbAudit.AppendLine();
                 }
-            }
-            catch (CSVReaderInvalidCSVException ex)
-            {
-                sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditErrorFound, ex.Message);
-                sbAudit.AppendLine();
-            }
-            catch (MyFlightbookValidationException ex)
-            {
-                sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditErrorFound, ex.Message);
-                sbAudit.AppendLine();
+                catch (MyFlightbookValidationException ex)
+                {
+                    sbAudit.AppendFormat(CultureInfo.CurrentCulture, Resources.LogbookEntry.csvAuditErrorFound, ex.Message);
+                    sbAudit.AppendLine();
+                }
             }
 
             return Status = CSVStatus.Broken;
