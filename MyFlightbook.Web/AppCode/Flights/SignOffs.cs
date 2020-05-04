220d4980a9c402ca08474b8a8257cbbc6e4cf0ae
ericberman <ericberman@users.noreply.github.com>
2020-05-03 17:08:54 -0700

    Issue #594: Make club and instructor/student requests HTML (using Markdown)

--- MyFlightbook.Web/AppCode/Flights/SignOffs.cs
+++ MyFlightbook.Web/AppCode/Flights/SignOffs.cs
@@ -609,7 +609,11 @@ namespace MyFlightbook.Instruction
 
                 string szRequestor = String.Format(CultureInfo.CurrentCulture, "{0} ({1})", pf.UserFullName, pf.Email);
 
-                msg.Body = szTemplate.Replace("<% Requestor %>", szRequestor).Replace("<% Role %>", szRole).Replace("<% ConfirmRoleLink %>", szCallBackUrl).Replace("<% ClubName %>", ClubToJoin == null ? string.Empty : ClubToJoin.Name);
+                msg.Body = String.Format(CultureInfo.InvariantCulture, @"<html>
+                        <head><head>
+                        <body>{0}</body>
+                </html>", szTemplate.Replace("<% Requestor %>", szRequestor).Replace("<% Role %>", szRole).Replace("<% ConfirmRoleLink %>", szCallBackUrl).Replace("<% ClubName %>", ClubToJoin == null ? string.Empty : ClubToJoin.Name).Linkify(false).Replace("\r\n", "<br />"));
+                msg.IsBodyHtml = true;
                 util.SendMessage(msg);
             }
         }
