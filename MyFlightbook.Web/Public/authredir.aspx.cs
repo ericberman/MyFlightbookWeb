f7a12fc9f698cc11679d8a05b3ff7b00dff54210
ericberman <ericberman@users.noreply.github.com>
2022-08-03 08:56:41 -0700

    Add an account destination to authredir

--- MyFlightbook.Web/Public/authredir.aspx.cs
+++ MyFlightbook.Web/Public/authredir.aspx.cs
@@ -7,145 +7,150 @@ using System.Web.UI;
 
 /******************************************************
  * 
- * Copyright (c) 2015-2020 MyFlightbook LLC
+ * Copyright (c) 2015-2022 MyFlightbook LLC
  * Contact myflightbook-at-gmail.com for more information
  *
 *******************************************************/
 
-public partial class Public_authredir : System.Web.UI.Page
+namespace MyFlightbook.PublicPages
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
-    private static string RedirForDest(string szDest, List<string> lstParams)
+    public partial class authredir : Page
     {
-        switch (szDest.ToUpperInvariant())
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
+        private static string RedirForDest(string szDest, List<string> lstParams)
         {
-            case "FLIGHTS":
-                return "~/Member/LogbookNew.aspx";
-            case "MINIFLIGHTS":
-                return "~/Member/MiniRecents.aspx";
-            case "PROFILE":
-                lstParams.Add("nolocalprefs=yes");
-                return "~/Member/EditProfile.aspx/pftPrefs";
-            case "DONATE":
-                return "~/Member/EditProfile.aspx/pftDonate";
-            case "ENDORSE":
-                return "~/Member/Training.aspx/instEndorsements";
-            case "PROGRESS":
-                return "~/Member/RatingProgress.aspx";
-            case "BADGES":
-                return "~/Member/Achievements.aspx";
-            case "STUDENTS":
-            case "STUDENTSFIXED":
-                return "~/Member/Training.aspx/instStudents";
-            case "INSTRUCTORS":
-            case "INSTRUCTORSFIXED":
-                return "~/Member/Training.aspx/instInstructors";
-            case "8710":
-                return "~/Member/8710Form.aspx";
-            case "MODELROLLUP":
-                return "~/Member/8710Form.aspx/Model";
-            case "TIMEROLLUP":
-                return "~/Member/8710Form.aspx/Time";
-            case "AIRCRAFTEDIT":
-                return "~/Member/EditAircraft.aspx";
-            case "AIRCRAFTSCHEDULE":
-                return "~/Member/ACSchedule.aspx";
-            case "FAQ":
-                return "~/Public/FAQ.aspx";
-            case "REQSIGS":
-                return "~/Member/RequestSigs.aspx";
-            case "FLIGHTREVIEW":
-                lstParams.Add("pane=flightreview");
-                return "~/Member/EditProfile.aspx/pftPilotInfo";
-            case "CERTIFICATES":
-                lstParams.Add("pane=certificates");
-                return "~/Member/EditProfile.aspx/pftPilotInfo";
-            case "MEDICAL":
-                lstParams.Add("pane=medical");
-                return "~/Member/EditProfile.aspx/pftPilotInfo";
-            case "DEADLINE":
-                lstParams.Add("pane=deadlines");
-                return "~/Member/EditProfile.aspx/pftPrefs";
-            case "CUSTOMCURRENCY":
-                lstParams.Add("pane=custcurrency");
-                return "~/Member/EditProfile.aspx/pftPrefs";
-            default:
-                return string.Empty;
-        }
-    }
-
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        string szDestErr = "~/Default.aspx";
-
-        string szUser = util.GetStringParam(Request, "u");
-        string szPass = util.GetStringParam(Request, "p");
-        string szDest = util.GetStringParam(Request, "d");
-
-        if (!MFBWebService.CheckSecurity(Request) ||
-            String.IsNullOrEmpty(szUser) || 
-            String.IsNullOrEmpty(szPass) || 
-            String.IsNullOrEmpty(szDest))
-            Response.Redirect(szDestErr);
-
-        // look for admin emulation in the form of 
-        string[] rgUsers = szUser.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-        string szEmulate = string.Empty;
-        if (rgUsers != null && rgUsers.Length == 2)
-        {
-            szEmulate = rgUsers[0];
-            szUser = rgUsers[1];
-        }
-
-        szUser = Membership.GetUserNameByEmail(szUser);
-
-        if (Membership.ValidateUser(szUser, szPass))
-        {
-            if (!String.IsNullOrEmpty(szEmulate))   // emulation requested - validate that the authenticated user is actually authorized!!!
+            switch (szDest.ToUpperInvariant())
             {
-                Profile pf = MyFlightbook.Profile.GetUser(szUser);
-                if (pf.CanSupport || pf.CanManageData)
-                {
-                    // see if the emulated user actually exists
-                    pf = MyFlightbook.Profile.GetUser(szEmulate);
-                    if (!pf.IsValid())
-                        throw new MyFlightbookException("No such user: " + szEmulate);
-                    szUser = szEmulate;
-                }
-                else
-                    throw new UnauthorizedAccessException();
+                case "FLIGHTS":
+                    return "~/Member/LogbookNew.aspx";
+                case "MINIFLIGHTS":
+                    return "~/Member/MiniRecents.aspx";
+                case "PROFILE":
+                    lstParams.Add("nolocalprefs=yes");
+                    return "~/Member/EditProfile.aspx/pftPrefs";
+                case "DONATE":
+                    return "~/Member/EditProfile.aspx/pftDonate";
+                case "ENDORSE":
+                    return "~/Member/Training.aspx/instEndorsements";
+                case "PROGRESS":
+                    return "~/Member/RatingProgress.aspx";
+                case "BADGES":
+                    return "~/Member/Achievements.aspx";
+                case "STUDENTS":
+                case "STUDENTSFIXED":
+                    return "~/Member/Training.aspx/instStudents";
+                case "INSTRUCTORS":
+                case "INSTRUCTORSFIXED":
+                    return "~/Member/Training.aspx/instInstructors";
+                case "8710":
+                    return "~/Member/8710Form.aspx";
+                case "MODELROLLUP":
+                    return "~/Member/8710Form.aspx/Model";
+                case "TIMEROLLUP":
+                    return "~/Member/8710Form.aspx/Time";
+                case "AIRCRAFTEDIT":
+                    return "~/Member/EditAircraft.aspx";
+                case "AIRCRAFTSCHEDULE":
+                    return "~/Member/ACSchedule.aspx";
+                case "FAQ":
+                    return "~/Public/FAQ.aspx";
+                case "REQSIGS":
+                    return "~/Member/RequestSigs.aspx";
+                case "FLIGHTREVIEW":
+                    lstParams.Add("pane=flightreview");
+                    return "~/Member/EditProfile.aspx/pftPilotInfo";
+                case "CERTIFICATES":
+                    lstParams.Add("pane=certificates");
+                    return "~/Member/EditProfile.aspx/pftPilotInfo";
+                case "MEDICAL":
+                    lstParams.Add("pane=medical");
+                    return "~/Member/EditProfile.aspx/pftPilotInfo";
+                case "DEADLINE":
+                    lstParams.Add("pane=deadlines");
+                    return "~/Member/EditProfile.aspx/pftPrefs";
+                case "CUSTOMCURRENCY":
+                    lstParams.Add("pane=custcurrency");
+                    return "~/Member/EditProfile.aspx/pftPrefs";
+                case "ACCOUNT":
+                    return "~/Member/EditProfile.aspx/pftAccount";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string szDestErr = "~/Default.aspx";
+
+            string szUser = util.GetStringParam(Request, "u");
+            string szPass = util.GetStringParam(Request, "p");
+            string szDest = util.GetStringParam(Request, "d");
+
+            if (!MFBWebService.CheckSecurity(Request) ||
+                String.IsNullOrEmpty(szUser) ||
+                String.IsNullOrEmpty(szPass) ||
+                String.IsNullOrEmpty(szDest))
+                Response.Redirect(szDestErr);
+
+            // look for admin emulation in the form of 
+            string[] rgUsers = szUser.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+            string szEmulate = string.Empty;
+            if (rgUsers != null && rgUsers.Length == 2)
+            {
+                szEmulate = rgUsers[0];
+                szUser = rgUsers[1];
             }
 
-            FormsAuthentication.SetAuthCookie(szUser, false);
-        }
+            szUser = Membership.GetUserNameByEmail(szUser);
 
-        List<string> lstParams = new List<string>();
+            if (Membership.ValidateUser(szUser, szPass))
+            {
+                if (!String.IsNullOrEmpty(szEmulate))   // emulation requested - validate that the authenticated user is actually authorized!!!
+                {
+                    Profile pf = Profile.GetUser(szUser);
+                    if (pf.CanSupport || pf.CanManageData)
+                    {
+                        // see if the emulated user actually exists
+                        pf = MyFlightbook.Profile.GetUser(szEmulate);
+                        if (!pf.IsValid())
+                            throw new MyFlightbookException("No such user: " + szEmulate);
+                        szUser = szEmulate;
+                    }
+                    else
+                        throw new UnauthorizedAccessException();
+                }
 
-        // BUGBUG: I got students/instructors reversed in iPhone.
-        if (Request.UserAgent.Contains("iPhone") || Request.UserAgent.Contains("iPad"))
-        {
-            if (String.Compare(szDest, "students", StringComparison.CurrentCultureIgnoreCase) == 0)
-                szDest = "instructors";
-            else if (String.Compare(szDest, "instructors", StringComparison.CurrentCultureIgnoreCase) == 0)
-                szDest = "students";
-        }
+                FormsAuthentication.SetAuthCookie(szUser, false);
+            }
 
-        szDest = RedirForDest(szDest, lstParams);
+            List<string> lstParams = new List<string>();
 
-        // this is something of a hack, but pass on any additional parameters
-        foreach (string szKey in Request.QueryString.Keys)
-            if (szKey != "u" && szKey != "p" && szKey != "d")
-                lstParams.Add(String.Format(CultureInfo.InvariantCulture, "{0}={1}", szKey, Request.Params[szKey]));
+            // BUGBUG: I got students/instructors reversed in iPhone.
+            if (Request.UserAgent.Contains("iPhone") || Request.UserAgent.Contains("iPad"))
+            {
+                if (String.Compare(szDest, "students", StringComparison.CurrentCultureIgnoreCase) == 0)
+                    szDest = "instructors";
+                else if (String.Compare(szDest, "instructors", StringComparison.CurrentCultureIgnoreCase) == 0)
+                    szDest = "students";
+            }
 
-        if (lstParams.Contains("naked=1"))
-            Session["IsNaked"] = true;
+            szDest = RedirForDest(szDest, lstParams);
 
-        if (szDest.Length == 0)
-            Response.Redirect(szDestErr);
-        else
-        {
-            string szUrlRedir = String.Format(CultureInfo.InvariantCulture, "javascript:window.top.location='{0}?{1}'", ResolveUrl(szDest), String.Join("&", lstParams.ToArray()));
-            Page.ClientScript.RegisterStartupScript(this.GetType(), "StartupRedir", szUrlRedir, true);
+            // this is something of a hack, but pass on any additional parameters
+            foreach (string szKey in Request.QueryString.Keys)
+                if (szKey != "u" && szKey != "p" && szKey != "d")
+                    lstParams.Add(String.Format(CultureInfo.InvariantCulture, "{0}={1}", szKey, Request.Params[szKey]));
+
+            if (lstParams.Contains("naked=1"))
+                Session["IsNaked"] = true;
+
+            if (szDest.Length == 0)
+                Response.Redirect(szDestErr);
+            else
+            {
+                string szUrlRedir = String.Format(CultureInfo.InvariantCulture, "javascript:window.top.location='{0}?{1}'", ResolveUrl(szDest), String.Join("&", lstParams.ToArray()));
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "StartupRedir", szUrlRedir, true);
+            }
         }
     }
 }
--- MyFlightbook.Web/Public/authredir.aspx.designer.cs
+++ MyFlightbook.Web/Public/authredir.aspx.designer.cs
@@ -7,17 +7,20 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-
-public partial class Public_authredir
+namespace MyFlightbook.PublicPages
 {
 
-    /// <summary>
-    /// form1 control.
-    /// </summary>
-    /// <remarks>
-    /// Auto-generated field.
-    /// To modify move field declaration from designer file to code-behind file.
-    /// </remarks>
-    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+    public partial class authredir
+    {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
 }
