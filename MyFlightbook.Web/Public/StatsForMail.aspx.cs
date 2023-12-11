1fa739241f1892563203325369dd0f19e7f458ea
Eric Berman <ericberman@users.noreply.github.com>
2023-12-11 09:20:07 -0800

    Return "Success" or throw an exception so we can see what might have happened if it doesn't run

--- MyFlightbook.Web/Public/StatsForMail.aspx.cs
+++ MyFlightbook.Web/Public/StatsForMail.aspx.cs
@@ -27,8 +27,13 @@ namespace MyFlightbook.PublicPages
                         string szURL = "~/mvc/Admin/Stats?fForEmail=true&naked=1".ToAbsoluteURL(Request.Url.Scheme, Request.Url.Host, Request.Url.Port).ToString();
                         byte[] rgdata = wc.DownloadData(szURL);
                         util.NotifyAdminEvent(String.Format(CultureInfo.CurrentCulture, "{0} site stats as of {1} {2}", Request.Url.Host, DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString()), System.Text.UTF8Encoding.UTF8.GetString(rgdata).Trim(), ProfileRoles.maskCanReport);
+                        Response.Write("Success");
+                        Response.Flush();
+                        Response.End();
                     }
                 }
+                else
+                    throw new UnauthorizedAccessException(String.Format(CultureInfo.CurrentCulture, "Attempt to hit StatsForMail from other than local machine.  Source: {0}", szIPThis));
             }
         }
     }
