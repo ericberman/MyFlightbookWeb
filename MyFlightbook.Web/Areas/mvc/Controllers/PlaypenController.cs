edcf33849b8a0c4a9a41485d6742eb7a28bc3dae
Eric Berman <ericberman@users.noreply.github.com>
2023-12-23 15:56:21 -0800

    Start a playpen MVC controller (mostly to start playing with drag & drop uploader)

---
+++ MyFlightbook.Web/Areas/mvc/Controllers/PlaypenController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/******************************************************
+ * 
+ * Copyright (c) 2023 MyFlightbook LLC
+ * Contact myflightbook-at-gmail.com for more information
+ *
+*******************************************************/
+
+namespace MyFlightbook.Web.Areas.mvc.Controllers
+{
+    public class PlaypenController : Controller
+    {
+        // GET: mvc/Playpen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
