33312decb4fdf9df6096b29ba659ec3efdcaae8c
Eric Berman <ericberman@users.noreply.github.com>
2023-12-27 15:31:24 -0800

    Migrate daynight (playpen) to MVC.  This leaves only flightdetails and ViewSharedLogbook with googlemaps ascx control any more!

--- MyFlightbook.Web/Areas/mvc/Controllers/PlaypenController.cs
+++ MyFlightbook.Web/Areas/mvc/Controllers/PlaypenController.cs
@@ -1,4 +1,7 @@
-﻿using MyFlightbook.Geography;
+﻿using MyFlightbook.Airports;
+using MyFlightbook.Geography;
+using MyFlightbook.Mapping;
+using MyFlightbook.SolarTools;
 using MyFlightbook.Telemetry;
 using System;
 using System.Collections.Generic;
@@ -18,6 +21,45 @@ namespace MyFlightbook.Web.Areas.mvc.Controllers
 {
     public class PlaypenController : AdminControllerBase
     {
+        #region DayNight
+        #region WebServices
+        public ActionResult AirportLookup(string szCode)
+        {
+            return SafeOp(() =>
+            {
+                airport[] rgAirports = new AirportList(szCode).GetAirportList();
+                return rgAirports.Length != 0 ? Json(rgAirports[0].LatLong) : throw new InvalidOperationException(Resources.Airports.errNoAirportsFound);
+            });
+        }
+        #endregion
+
+        #region Endpoints
+        [HttpGet]
+        public ActionResult DayNight(double? latitude, double? longitude, DateTime? date)
+        {
+            GoogleMap googleMap = new GoogleMap("divAirport", GMap_Mode.Dynamic)
+            {
+                ClickHandler = "function (point) {clickForAirport(point.latLng);}"
+            };
+            googleMap.Options.ZoomFactor = GMap_ZoomLevels.US;
+            ViewBag.Map = googleMap;
+            if (latitude.HasValue && longitude.HasValue && date.HasValue)
+            {
+                DateTime dtUTC = new DateTime(date.Value.Year, date.Value.Month, date.Value.Day, 0, 0, 0, DateTimeKind.Utc);
+                SunriseSunsetTimes sst = new SunriseSunsetTimes(dtUTC, latitude.Value, longitude.Value);
+                ViewBag.dtUTC = dtUTC;
+                ViewBag.sunriseUTC = sst.Sunrise;
+                ViewBag.sunsetUTC = sst.Sunset;
+                ViewBag.dtUTCDisplay = new DateTime(date.Value.Year, date.Value.Month, date.Value.Day, 0, 0, 0, DateTimeKind.Utc).ToLongDateString();
+                googleMap.Options.MapCenter = new LatLong(latitude.Value, longitude.Value);
+            }
+            ViewBag.latitude = latitude ?? 0;
+            ViewBag.longitude = longitude ?? 0;
+            return View("dayNight");
+        }
+        #endregion
+        #endregion
+
         #region Merge Telemetry
         #region Session Management for telemetry being merged
         private string SessionKeyBase { get { return User.Identity.Name + "telemmerge"; } }
