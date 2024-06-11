dd11b75d9eac6812bfb3f2fa7948cf7a8bf98347
Eric Berman <ericberman@users.noreply.github.com>
2024-06-11 13:03:52 -0700

    updqte minimal database

--- MyFlightbook.Web/Support/2023-01-21 - Issue1050 FK for UserAircraft.sql
+++
@@ -1,6 +0,0 @@
-﻿ALTER TABLE `logbook`.`flights` 
-ADD CONSTRAINT `fkuseraircraft`
-  FOREIGN KEY (`idaircraft` , `username`)
-  REFERENCES `logbook`.`useraircraft` (`idAircraft` , `userName`)
-  ON DELETE RESTRICT
-  ON UPDATE CASCADE;
