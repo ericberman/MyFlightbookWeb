df1d417b951d0d3e165b23d3270238b3cc11cf1c
Eric Berman <ericberman@users.noreply.github.com>
2025-07-07 13:38:38 -0700

    Clean up some ore styling in modeleditor, a few other places.

--- MyFlightbook.Web/AppCode/Utility/util.cs
+++ MyFlightbook.Web/AppCode/Utility/util.cs
@@ -64,7 +64,7 @@ namespace MyFlightbook
 
         public const string keySessLastNewFlight = "sessNewFlightID";
 
-        public const int StyleSheetVer = 66;
+        public const int StyleSheetVer = 67;
 
         public static string BaseStylesheet
         {
